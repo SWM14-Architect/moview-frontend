439705739d547358642264022b8e15adb536c9a1
yang-yeongjun <dasd412@naver.com>
2023-07-10 16:16:44 +0900

    feat : add css in end page

--- src/pages/end.jsx
+++ src/pages/end.jsx
@@ -1,30 +1,53 @@
 import React, { useRef, useEffect } from "react";
-import 'chart.js/auto';
-import { Radar,Bar } from 'react-chartjs-2';
+import "chart.js/auto";
+import { Radar, Bar } from "react-chartjs-2";
+import Header from "../components/header";
+import Footer from "../components/footer";
+import "../styles/outer-div.css";
+import "../styles/end.css";
+import "../styles/button.css";
 
 const RadarChart = () => {
   const data = {
-    labels: ['Category A', 'Category B', 'Category C', 'Category D', 'Category E'],
+    labels: [
+      "Category A",
+      "Category B",
+      "Category C",
+      "Category D",
+      "Category E",
+    ],
     datasets: [
       {
         data: [4, 3, 5, 2, 4],
-        backgroundColor: 'rgba(0, 123, 255, 0.25)',
-        borderColor: 'blue',
+        backgroundColor: "rgba(0, 123, 255, 0.25)",
+        borderColor: "blue",
         borderWidth: 2,
+        label: "내 점수 영역"
       },
     ],
   };
-  
-  return <Radar data={data} options={{ title: { display: true, text: 'Radar Chart' }}} />;
+
+  return (
+    <Radar
+      data={data}
+      options={{ title: { display: true, text: "Radar Chart" } }}
+    />
+  );
 };
 
 const BarChart = () => {
   const data = {
-    labels: ['Category A', 'Category B', 'Category C', 'Category D'],
+    labels: ["Category A", "Category B", "Category C", "Category D"],
     datasets: [
       {
         data: [10, 15, 7, 12],
-        backgroundColor: 'blue',
+        backgroundColor: "blue",
+        label:"내 점수"
+      },
+      {
+        data: [11,20,13,15],
+        backgroundColor: "red",
+        label: "다른 사람 평균 점수"
       },
     ],
   };
@@ -33,31 +56,47 @@ const BarChart = () => {
     <Bar
       data={data}
       options={{
-        title: { display: true, text: 'Horizontal Bar Chart' },
+        title: { display: true, text: "Horizontal Bar Chart" },
         scales: { xAxes: [{ ticks: { beginAtZero: true } }] },
       }}
     />
   );
 };
 
-const App = () => {
+const End = () => {
+  const handleSubmit = () => {
+    // 제출 버튼이 눌렸을 때의 동작을 정의합니다.
+  };
+
   return (
-    <div>
-      <h2>영역별 점수</h2>
-      <RadarChart />
-      <hr />
-      <h2>다른 사람과의 점수 비교</h2>
-      <BarChart />
-      <hr />
+    <div className="outer-div">
+      <Header />
+
+      <div className="chart">
+        <h2>영역별 점수</h2>
+        <RadarChart />
+        <hr />
+        <h2>다른 사람과의 점수 비교</h2>
+        <BarChart />
+        <hr />
+      </div>
+
       <h2>총 평가</h2>
-      <textarea placeholder="좋은 점" />
-      <textarea placeholder="아쉬운 점" />
-      <hr />
-      <input type="text" placeholder="기타 원하시는 것이 있으면 입력해주세요~~" />
-      <hr />
-      <button>제출</button>
+      <div className="end-results">
+        <textarea placeholder="좋은 점" disabled />
+        <textarea placeholder="아쉬운 점" disabled />
+        <hr />
+        <textarea
+          placeholder="기타 원하시는 것이 있으면 입력해주세요~~"
+        />
+      </div>
+
+      <button className="big-button" onClick={handleSubmit}>
+        평가 완료
+      </button>
+      <Footer />
     </div>
   );
 };
 
-export default App;
+export default End;
