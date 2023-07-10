b03cabf495ea7a12d855482312193d4e3fb41239
yang-yeongjun <dasd412@naver.com>
2023-07-10 11:31:17 +0900

    refactor : header and footer react style code moved to css file

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,24 +1,12 @@
 import React from "react";
+import "../styles/header.css";
 
 function Header() {
   return (
-    <header className="custom-header" style={headerStyle}>
+    <header className="header">
       취준생을 위한 면접 연습 서비스
     </header>
   );
 }
 
 export default Header;
-
-const headerStyle = {
-  background: "#f2f2f2",
-  padding: "20px",
-  textAlign: "center",
-  fontSize: "24px",
-  fontWeight: "bold",
-  color: "#333",
-  textShadow: "1px 1px 2px rgba(0, 0, 0, 0.1)",
-  width: "70%",
-  margin: "0 auto",
-  borderBottom: "1px solid #ccc",
-};
--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import EndPage from "./pages/end";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Input />
+    <Chat />
   </React.StrictMode>
 );
 
