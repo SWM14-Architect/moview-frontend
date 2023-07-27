d903f10cd8fbb0bec899821702f284fd399bc7f0
JangSeHun <westreed@naver.com>
2023-07-27 15:07:24 +0900

    refactor: footer 수정

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -2,10 +2,12 @@ import React from "react";
 
 function Footer() {
   return (
-    <footer>
+    <footer style={{borderTop:"#e4ebf3 2px solid"}}>
       <div className={`container`}>
-        <div>
-          <div>Copyright © 2023 SWM Architect Team All rights reserved.</div>
+        <div style={{width:"100%"}}>
+          <div style={{textAlign:"center", marginBottom:"20px", fontSize:"2em"}}>Footer</div>
+          <div style={{height:"1px", backgroundColor:"#e4ebf3", margin:"10px"}} />
+          <div style={{textAlign:"center", marginBottom:"20px", color:"#676767"}}>Copyright © 2023 SWM Architect Team All rights reserved.</div>
         </div>
       </div>
     </footer>
