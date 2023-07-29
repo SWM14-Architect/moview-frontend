67d24a7ff225d8e1a8d06779090e9f2c67677b7f
JangSeHun <westreed@naver.com>
2023-07-29 23:23:58 +0900

    feat: useInterval Custom Hook 추가
    일정 시간마다 callback을 실행해줄 수 있는
    userInterval Custom Hook을 추가함.

---
+++ src/utils/useInterval.js
@@ -0,0 +1,24 @@
+import {useEffect, useRef} from "react";
+
+// Custom Hook for setInterval
+export const useInterval = (callback, interval, memo) => {
+  const savedCallback = useRef(null);
+
+  useEffect(() => {
+    savedCallback.current = callback;
+  }, memo);
+
+  useEffect(() => {
+    function tick() {
+      if (savedCallback.current) {
+        savedCallback.current();
+      }
+    }
+
+    let id = setInterval(tick, interval);
+    return () => {
+      clearInterval(id);
+      savedCallback.current = null;
+    }
+  }, [interval]);
+};
