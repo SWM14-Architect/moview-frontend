18a38f534f64f8f1a72d49dec076393efb5159d1
JangSeHun <westreed@naver.com>
2023-08-02 16:10:02 +0900

    refactor: room Atom
    데이터가 유지될 수 있게 persist 설정을 추가하고,
    interivewData의 초기값은 상수로 빼서 관리함.

---
+++ src/constants/interviewRoomConst.js
@@ -0,0 +1,10 @@
+
+const INTERVIEW_INPUT_FORM_DEFAULT_VALUE = {
+  "intervieweeName": "소마인",
+  "interviewTargetCompany": "삼성 SDS",
+  "interviewTargetPosition": "Backend Developer",
+  "interviewRecruitment": "",
+  "interviewCoverLetters": [],
+};
+
+export { INTERVIEW_INPUT_FORM_DEFAULT_VALUE };
--- src/store/room_atom.js
+++ src/store/room_atom.js
@@ -1,17 +1,17 @@
 import {atom} from "recoil";
+import {recoilPersist} from "recoil-persist";
+import {INTERVIEW_INPUT_FORM_DEFAULT_VALUE} from "../constants/interviewRoomConst";
+
+const {persistAtom} = recoilPersist();
 
 export const roomIdAtom = atom({
   key: "roomIDState",
-  default: "interviewFeedback",
+  default: "interviewInput",
+  effects_UNSTABLE: [persistAtom],
 });
 
 export const interviewDataAtom = atom({
   key: "interviewDataState",
-  default: {
-    "intervieweeName": "소마인",
-    "interviewTargetCompany": "삼성 SDS",
-    "interviewTargetPosition": "Backend Developer",
-    "interviewRecruitment": "",
-    "interviewCoverLetters": [],
-  },
+  default: INTERVIEW_INPUT_FORM_DEFAULT_VALUE,
+  effects_UNSTABLE: [persistAtom],
 });
