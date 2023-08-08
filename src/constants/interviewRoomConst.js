2a171fe8480c84b0523cac8e4be2918b8b139437
JangSeHun <westreed@naver.com>
2023-08-08 17:31:47 +0900

    fix: Input Form Default Value
    Inputform 상수값에 테스트데이터가 남아있던 부분을
    수정했습니다.

--- src/constants/interviewRoomConst.js
+++ src/constants/interviewRoomConst.js
@@ -1,8 +1,8 @@
 
 const INTERVIEW_INPUT_FORM_DEFAULT_VALUE = {
-  "intervieweeName": "소마인",
-  "interviewTargetCompany": "삼성 SDS",
-  "interviewTargetPosition": "Backend Developer",
+  "intervieweeName": "",
+  "interviewTargetCompany": "",
+  "interviewTargetPosition": "",
   "interviewRecruitment": "",
   "interviewCoverLetters": [],
 };
