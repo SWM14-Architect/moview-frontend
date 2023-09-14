d8587f7f103cd20707712ba4ca57fc21131e88fe
yang-yeongjun <dasd412@naver.com>
2023-09-14 13:10:55 +0900

    style : react 문법에 맞게 스타일 적용 + 로딩 바는 애니메이션 css에 옮김.

--- src/pages/oauth.jsx
+++ src/pages/oauth.jsx
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import style from "../styles/oauth.module.css";
+import KakaoButton from "../assets/kakaolink_btn_small.png";
 
 const OAuth = () => {
   const [isLogged, setIsLogged] = useState(false);
@@ -6,44 +8,54 @@ const OAuth = () => {
   const [thumbnailSrc, setThumbnailSrc] = useState("");
 
   const handleLogin = () => {
-
+    // 로그인 로직
   };
 
   const handleLogout = () => {
-
+    // 로그아웃 로직
   };
 
   return (
-    <div className="Wrapper">
-      <div className="title">Kakao OAuth Login</div>
-      <div className="info">
-        Kakao OAuth
-      </div>
+    <div className={`${style.oauth}`}>
+      <div className={`${style.oauth_body}`}>
+        <div className={`${style.Wrapper}`}>
+          <div className={`${style.title}`}>Kakao OAuth Login</div>
+          <div className={`${style.info}`}>Kakao OAuth</div>
 
-      {!isLogged ? (
-        <div id="kakao" className="kakao_button" onClick={handleLogin}>
-          <img src="/path/to/kakaolink_btn_small.png" alt="kakao oauth" />
-          <span>카카오 로그인</span>
+          {!isLogged ? (
+            <div
+              id="kakao"
+              className={`${style.kakao_button}`}
+              onClick={handleLogin}
+            >
+              <img src={KakaoButton} />
+              <span>카카오 로그인</span>
+            </div>
+          ) : (
+            <>
+              <img
+                id="thumbnail"
+                src={thumbnailSrc}
+                className={`${style.thumbnail}`}
+                alt="User Thumbnail"
+              />
+              <div id="nickname" className={`${style.nickname}`}>
+                {nickname}
+              </div>
+              <div
+                id="logout"
+                className={`${style.logout}`}
+                onClick={handleLogout}
+              >
+                <span>로그아웃</span>
+              </div>
+            </>
+          )}
+          {/* Loading part, hidden by default, toggle as needed */}
+          <div id="loading" className={`${style.loading}`}>
+            <div className={`${style.loading_circle}`}></div>
+          </div>
         </div>
-      ) : (
-        <>
-          <img
-            id="thumbnail"
-            src={thumbnailSrc}
-            className="thumbnail"
-            alt="User Thumbnail"
-          />
-          <div id="nickname" className="nickname">
-            {nickname}
-          </div>
-          <div id="logout" className="logout" onClick={handleLogout}>
-            <span>로그아웃</span>
-          </div>
-        </>
-      )}
-      {/* Loading part, hidden by default, toggle as needed */}
-      <div id="loading" className="loading">
-        <div className="loading_circle"></div>
       </div>
     </div>
   );
