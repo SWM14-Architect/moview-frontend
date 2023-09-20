a75f48e05f65acb59a11fc910d12c6dd07de1bf4
yang-yeongjun <dasd412@naver.com>
2023-09-20 15:14:27 +0900

    feat : react google analytics 적용

--- src/index.js
+++ src/index.js
@@ -18,9 +18,17 @@ import "./styles/toast.css";
 import Main from "./pages/main";
 import InterviewRoom from "./pages/interviewRoom";
 import ToastContainerComponent from "./utils/toastContainer";
+import ReactGA from "react-ga4";
+import RouteChangeTracker from "./RouteChangeTracker";
 
+// 구글 애널리틱스 운영서버만 적용
+if (process.env.REACT_APP_GOOGLE_ANALYTICS) {
+  ReactGA.initialize(process.env.REACT_APP_GOOGLE_ANALYTICS);
+}
 
 function Index(){
+  RouteChangeTracker();
+  
   const router = createBrowserRouter([
     {
       path: "/", element: <App />,
