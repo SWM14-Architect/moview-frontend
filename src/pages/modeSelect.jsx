7a163cd6347d14756711be2303ff4d66e3376426
yang-yeongjun <dasd412@naver.com>
2023-09-30 17:47:18 +0900

    feat : 모드 셀렉트 화면에서 이동 버튼 추가

--- src/pages/modeSelect.jsx
+++ src/pages/modeSelect.jsx
@@ -1,11 +1,28 @@
-import React, { useCallback, useState, useRef } from "react";
+import React, { useCallback, useRef } from "react";
 import { ScrollToTop } from "../utils/scrollRestoration";
 import style from "../styles/modelSelect.module.css";
 import DevilIcon from "../assets/devil_icon.svg";
 import CatIcon from "../assets/cat.svg";
+import { useNavigate } from "react-router-dom";
+import { useRecoilState } from "recoil";
+import { roomIdAtom } from "../store/interviewRoomAtom";
 
 function ModeSelect() {
   ScrollToTop();
+  const navigate = useNavigate();
+  const [, setRoomID] = useRecoilState(roomIdAtom);
+
+  const handleButtonClick = (e) => {
+    e.preventDefault();
+    setRoomID("interviewInput");
+    navigate("/room");
+  };
+
+  const handleButtonClickOfLight = (e) => {
+    e.preventDefault();
+    setRoomID("interviewLight");
+    navigate("/room");
+  };
 
   return (
     <section style={{ backgroundColor: "#f4f7fb", flex: 1 }}>
@@ -14,24 +31,66 @@ function ModeSelect() {
         <div className={`fadeInUpEffect`}>
           <div className={`${style.header}`}>모드 선택</div>
         </div>
+
+        {/*연습 모드 설명 컴포넌트*/}
         <ModeExplanation
           item={
-            "자소서 없이 직무 질문만 가볍게 연습하고 싶다면, Light 모드를 선택해주세요! 반복연습해서 당신의 지식으로 만들어 보자구요!"
+            "자소서 없이 직무 질문만 가볍게 연습하고 싶다면, 연습 모드를 선택해주세요! 반복연습해서 당신의 지식으로 만들어 보자구요!"
           }
           iconSrc={CatIcon}
         />
 
+        {/*실전 모드 설명 컴포넌트*/}
         <ModeExplanation
           item={
-            "면접이 얼마 안 남았다면, 실전 모드를 선택해서 합격률을 높이세요! 단, 실제 면접에는 자소서를 보고 진행하기 때문에 자소서 입력이 필요합니다."
+            "면접이 얼마 안 남았다면, 실전 모드를 선택해서 실제 면접에 나올 수 있는 질문들을 대비하세요! (단, 자소서 입력이 필요합니다.)"
           }
           iconSrc={DevilIcon}
         />
+
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+
+        {/*연습 모드 버튼*/}
+          <button
+            className={`blueButton`}
+            style={{
+              marginTop: "60px",
+              marginBottom: "15px",
+              marginRight: "20px",
+              width: "150px",
+              borderRadius: "10px",
+            }}
+            onClick={(e) => handleButtonClickOfLight(e)}
+          >
+            연습
+          </button>
+
+        {/*실전 모드 버튼*/}
+          <button
+            className={`redButton`}
+            style={{
+              marginTop: "60px",
+              marginBottom: "15px",
+              width: "150px",
+              borderRadius: "10px",
+            }}
+            onClick={(e) => handleButtonClick(e)}
+          >
+            실전
+          </button>
+        </div>
       </div>
     </section>
   );
 }
 
+// @param item : textarea에 넣을 텍스트 값, iconSrc: icon asset 주소
 function ModeExplanation({ item, iconSrc }) {
   return (
     <div className={`${style.input_form}`}>
@@ -68,9 +127,5 @@ function TextareaForm({ item }) {
     />
   );
 }
-
-function LightMode() {}
-
-function HeavyMode() {}
 
 export default ModeSelect;
