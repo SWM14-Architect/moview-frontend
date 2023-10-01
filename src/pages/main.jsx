50bc2e80703901073a6e5564bc1c6334dcbf03bb
yang-yeongjun <dasd412@naver.com>
2023-10-01 16:49:24 +0900

    design: hero 이미지 변경

--- src/pages/main.jsx
+++ src/pages/main.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import MainImage from "../assets/recruitment-6838250_1920.png";
+import MainImage from "../assets/pexels-tima-miroshnichenko.jpg";
 import style from "../styles/main.module.css";
 import {useTitle} from "../utils/useTitle";
 import {SERVICE_TITLE} from "../constants/serviceConst";
@@ -20,11 +20,11 @@ function FirstSection(props){
         <div>을 느끼고 계신가요?</div>
       </TypeIt>
       <img className={`fadeInUpEffect animation-delay-1`} src={MainImage} alt="main" style={{width:"100%", marginBottom:"2em"}} />
-      <div className={`fadeInUpEffect animation-delay-2 ${style.title_content}`}>
+      {/* <div className={`fadeInUpEffect animation-delay-2 ${style.title_content}`}>
         더 이상 면접 질문에 당황하지 마세요!
         <br/>
         면접자의 자소서와 원하는 회사의 채용 공고를 분석하여 개인화된 질문 리스트를 제공합니다. 현실감 넘치는 면접 시뮬레이션을 통해 진짜 면접에 대비하세요.
-      </div>
+      </div> */}
       <div className={`line fadeInUpEffect animation-delay-3`} />
       <div className={`fadeInUpEffect animation-delay-3 ${style.title_content_start}`}>
       </div>
@@ -79,8 +79,9 @@ function SecondSection(){
       <div className={`layout-flex-grid-2`}>
         {featureComponent("자소서 및 채용 공고 분석", "면접자의 자소서와 원하는 회사의 채용 공고를 꼼꼼하게 분석합니다. 이 분석을 통해, 어떠한 질문도 예상하고 준비할 수 있게 도와줍니다.")}
         {featureComponent("개인화된 질문 리스트 생성", "분석된 결과를 토대로, 개인화된 질문 리스트를 제공합니다. 이를 통해 면접에서 당황하지 않고, 실제 면접에서 자주 묻는 질문에 대해 미리 준비할 수 있게 합니다.")}
-        {featureComponent("개인화된 역량 매칭", "나의 경험과 역량이 회사의 요구사항과 어떻게 연결될 수 있는지를 명확하게 이해하도록 도와줍니다. 이를 통해 더 효과적으로 자신의 역량을 강조하고, 회사가 원하는 인재상에 부합하는지 확인할 수 있습니다.")}
         {featureComponent("현실감 있는 면접 시뮬레이션", "실제 면접을 대비하기 위한 현실감 넘치는 면접 시뮬레이션을 제공합니다. 이 시뮬레이션은 사용자가 실제 면접 상황을 체험하고, 면접 과정에서 발생할 수 있는 다양한 변수들에 대비할 수 있게 해줍니다.")}
+        {featureComponent("면접 키워드 중심 면접 연습", "직무 기술 면접에서 나올 수 있는 키워드를 반복 연습해서 합격률을 높여봐요!")}
+
       </div>
     </div>
   );
