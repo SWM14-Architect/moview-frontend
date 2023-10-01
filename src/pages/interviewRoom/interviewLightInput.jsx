c239a4d6509f438da294bf31cf7c49597f436b95
yang-yeongjun <dasd412@naver.com>
2023-10-01 15:08:06 +0900

    feat : light mode 화면에 렌더링 로직, 입력 필터링 추가.

--- src/pages/interviewRoom/interviewLightInput.jsx
+++ src/pages/interviewRoom/interviewLightInput.jsx
@@ -1,22 +1,40 @@
 import React, { useCallback, useEffect, useRef, useState } from "react";
 import style from "../../styles/interviewInput.module.css";
 import { useRecoilState } from "recoil";
+import { ScrollToTop } from "../../utils/scrollRestoration";
+import { toast } from "react-toastify";
+import { userNicknameAtom } from "../../store/userAtom";
+import { chatHistoryAtom } from "../../store/interviewChatAtom";
+import { loadingAtom, loadingMessageAtom } from "../../store/loadingAtom";
 import {
   interviewDataAtom,
   interviewIdAtom,
   interviewStateAtom,
   roomIdAtom,
 } from "../../store/interviewRoomAtom";
-import { ScrollToTop } from "../../utils/scrollRestoration";
-import { toast } from "react-toastify";
 
 function InterviewLightInput() {
   ScrollToTop();
 
+  const [, setIsLoading] = useRecoilState(loadingAtom);
+  const [, setLoadingMessage] = useRecoilState(loadingMessageAtom);
+  // 사용자의 화면을 변경하기 위한 RoomID
+  const [, setRoomID] = useRecoilState(roomIdAtom);
+  // 사용자에게 입력받은 데이터를 전역상태로 저장
+  const [, setInterivewData] = useRecoilState(interviewDataAtom);
+  // 채팅방 대화 기록 (이전에 저장된 대화를 삭제하기 위해서 setter만 불러옴)
+  const [, setChatHistory] = useRecoilState(chatHistoryAtom);
+  // Interview ID
+  const [, setInterviewId] = useRecoilState(interviewIdAtom);
+  // 클라이언트 상태 관리
+  const [, setInterviewState] = useRecoilState(interviewStateAtom);
+  // 사용자 닉네임
+  const [userNickname] = useRecoilState(userNicknameAtom);
+
   // 사용자에게서 입력받는 데이터들
   const [interviewTargetCompany, setInterviewTargetCompany] = useState("");
   const [interviewTargetPosition, setInterviewTargetPosition] = useState("");
-  const [interviewRecruitment, setInterviewRecruitment] = useState("");
+  const [interviewTargetKeyword, setInterviewKeyword] = useState("");
 
   function handleInterviewCompanyChange(e) {
     setInterviewTargetCompany(e.target.value);
@@ -26,12 +44,39 @@ function InterviewLightInput() {
     setInterviewTargetPosition(e.target.value);
   }
 
-  function handleInterviewRecruitmentChange(e) {
-    setInterviewRecruitment(e.target.value);
+  function handleInterviewKeyword(e) {
+    setInterviewKeyword(e.target.value);
   }
 
   function handleNextButtonClick(e) {
     e.preventDefault();
+
+    const toastWarning = (text) => {
+      // toast에 공통 옵션을 줄 수도 있어서 함수화했습니다.
+      toast.warn(text, {});
+    };
+
+    // 입력 정보 필터
+    if (interviewTargetCompany === "")
+      return toastWarning("지원하고자 하는 회사를 입력해주세요.");
+    if (interviewTargetPosition === "")
+      return toastWarning("지원하고자 하는 직군을 입력해주세요.");
+
+    if (interviewTargetKeyword === "")
+      return toastWarning("연습하고 싶은 질문 키워드를 입력해주세요.");
+
+    setInterivewData({
+      intervieweeName: userNickname,
+      interviewTargetCompany: interviewTargetCompany,
+      interviewTargetPosition: interviewTargetPosition,
+      interviewTargetKeyword: interviewTargetKeyword,
+    });
+
+    // 로딩 렌더링
+    setIsLoading(true);
+    setLoadingMessage(
+      "잠시후 면접이 시작됩니다. 대기 시간은 약 3~6초 정도입니다!"
+    );
   }
 
   return (
@@ -42,29 +87,30 @@ function InterviewLightInput() {
           <InputComponent
             title={"회사"}
             placeholder={"지원하고자 하는 회사를 입력하세요."}
-            item={"hi"}
+            item={interviewTargetCompany}
             onChange={handleInterviewCompanyChange}
           />
           <InputComponent
             title={"직군"}
             placeholder={"지원하고자 하는 직군을 입력하세요."}
-            item={"hi"}
+            item={interviewTargetPosition}
             onChange={handleInterviewPositionChange}
           />
         </div>
         <div className={`fadeInUpEffect animation-delay-1`}>
           <TextareaComponent
             title={"면접 질문 키워드"}
-            placeholder={"연습하고 싶은 면접 질문 키워드를 입력하세요. (여러 개 가능)"}
-            item={"hi"}
-            onChange={handleInterviewRecruitmentChange}
+            placeholder={
+              "연습하고 싶은 면접 질문 키워드를 입력하세요. (여러 개 가능. 권장은 2~3개입니다.)"
+            }
+            item={interviewTargetKeyword}
+            onChange={handleInterviewKeyword}
           />
         </div>
         <div
           className={`fadeInUpEffect animation-delay-2`}
           style={{ margin: "10px" }}
-        >
-        </div>
+        ></div>
         <div
           className={`fadeInUpEffect animation-delay-2`}
           style={{ display: "flex", justifyContent: "center" }}
