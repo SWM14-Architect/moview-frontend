506233348098ce09f033e12b024816fa0244c4e2
hys <ssfic3380@gmail.com>
2023-10-10 15:31:44 +0900

    fix: 녹음 오류 수정

--- src/utils/audioRecorder.js
+++ src/utils/audioRecorder.js
@@ -5,21 +5,19 @@ import style from "../styles/interviewChat.module.css";
 
 export default function AudioRecorder({ className, canNotPlayerSpeaking, onSTTResult }) {
   const [mediaRecorder, setMediaRecorder] = useState(null); // 녹음에 사용될 MediaRecorder 객체를 저장
-  const [audioDuration, setAudioDuration] = useState(0); // 녹음이 시작된 시간을 저장
-  const [, setMicAccessDenied] = useState(false); // 마이크 접근 권한이 없는지 여부를 저장
 
   useEffect(() => {
     // 컴포넌트가 마운트 될 때 실행될 코드 (여기선 비워둠)
 
-    return () => {
+    return (() => {
       // 컴포넌트가 언마운트 될 때 실행될 코드
       if (mediaRecorder) {
-        mediaRecorder.stop();
         mediaRecorder.stream.getTracks().forEach(track => track.stop());
+        mediaRecorder.stop();
       }
-    };
+    });
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);  // 의존성 배열은 비워두어 컴포넌트 마운트/언마운트 시에만 실행되게 함
+  }, [mediaRecorder]);  // 의존성 배열은 비워두어 컴포넌트 마운트/언마운트 시에만 실행되게 함
 
   const startRecording = async () => {
     try {
@@ -28,7 +26,6 @@ export default function AudioRecorder({ className, canNotPlayerSpeaking, onSTTRe
 
       // MediaRecorder 객체 생성
       const newMediaRecorder = new MediaRecorder(stream, { mimeType: 'audio/webm' });
-      setAudioDuration(0);
 
       // 녹음 중에 데이터가 생성되면 audioChunks에 저장
       let localAudioChunks = [];
@@ -37,8 +34,9 @@ export default function AudioRecorder({ className, canNotPlayerSpeaking, onSTTRe
       };
 
       // 녹음이 시작될 때의 타임스탬프 저장 (너무 짧은 음성이 녹음되는 것을 방지하기 위함)
+      let audioDuration;
       newMediaRecorder.onstart = () => {
-        setAudioDuration(Date.now());
+        audioDuration = Date.now();
       };
 
       newMediaRecorder.onstop = () => {
@@ -66,16 +64,14 @@ export default function AudioRecorder({ className, canNotPlayerSpeaking, onSTTRe
       // 녹음 시작
       newMediaRecorder.start();
       setMediaRecorder(newMediaRecorder);
-      setMicAccessDenied(false);  // 마이크 접근이 성공하면 상태를 false로 설정
     } catch (err) {
       toast.error(`마이크 접근이 거부되었습니다. 마이크 권한을 허용해주세요.`, {});
-      setMicAccessDenied(true);  // 마이크 접근이 거부되면 상태를 true로 설정
     }
   };
 
   const stopRecording = () => {
-    mediaRecorder.stop(); // 녹음 중지
     mediaRecorder.stream.getTracks().forEach(track => track.stop()); // 미디어 디바이스에 대한 접근 중지
+    mediaRecorder.stop(); // 녹음 중지
   };
 
   const sendDataToServer = (base64Data) => {
