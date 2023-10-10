b84b2252e7b1fbd79ea0a65b82881f461fad5c7c
hys <ssfic3380@gmail.com>
2023-10-10 12:52:46 +0900

    remove: 사용 되지 않는 import문 제거

--- src/pages/interviewRoom/interviewLightInput.jsx
+++ src/pages/interviewRoom/interviewLightInput.jsx
@@ -1,4 +1,4 @@
-import React, {useCallback, useEffect, useRef, useState} from "react";
+import React, {useCallback, useRef, useState} from "react";
 import style from "../../styles/interviewInput.module.css";
 import { useRecoilState } from "recoil";
 import {
